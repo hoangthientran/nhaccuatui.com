0523a4d0da45a0db30fdfaa0f5a7448799bfe6f2
hoangthientran <hoangthien.tran@m.citynow.vn>
2021-05-06 17:55:34 +0700

    complete last ddsd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+// libs
 import React from "react";
+// others
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>hello </h2>
+      <h2>hello asd </h2>
     </div>
   );
 }
