4c6b63e14ea597313745a4407f369838cd48a777
hoangthientran <hoangthien.tran@m.citynow.vn>
2021-05-18 07:25:17 +0700

    Chia route thanh cong

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+// libs
 import React from "react";
-import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+// components
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./components/NotFound";
 import Footer from "./components/Footer";
+// other
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
-      <Home />
+      <Switch>
+        <Redirect from="/home" to="/" exact />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
---
+++ src/pages/About/index.jsx
@@ -0,0 +1,12 @@
+// libs
+import React from "react";
+// others
+import "./style.scss";
+
+const About = () => (
+  <div className="box-about-wrapper">
+    <h1>ABOUT</h1>
+  </div>
+);
+
+export default About;
